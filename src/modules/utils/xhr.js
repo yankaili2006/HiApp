e17e708cea933022c68e378c498d07b4633a46da
afa007 <582843680@qq.com>
2017-01-14 13:21:33 +0800

    xwl method add

--- src/modules/utils/xhr.js
+++ src/modules/utils/xhr.js
@@ -29,6 +29,73 @@ module.exports = {
         return apiServer.replace(/&$/gi, '');
     },
 
+    //讯物联URL
+    getRequestURLForXWL: function(options){
+        var query = options.query || {};
+        var func = options.func || '';
+
+        var apiServer = 'appbusi/' + func +
+            (appFunc.isEmpty(query) ? '' : '?');
+
+        var name;
+        for(name in query){
+            apiServer += name + '=' + query[name] + '&';
+        }
+
+        return apiServer.replace(/&$/gi, '');
+    },
+
+    //讯物联调用ajax
+    simpleCallForXWL : function(options, callback){
+        var that = this;
+
+        options = options || {};
+        options.data = options.data ? options.data : '';
+
+        //If you access your server api ,please user `post` method.
+
+        if(appFunc.isPhonegap()){
+            //Check network connection
+            var network = networkStatus.checkConnection();
+            if(network === 'NoNetwork'){
+
+                hiApp.alert(i18n.error.no_network, function () {
+                    hiApp.hideIndicator();
+                    hiApp.hidePreloader();
+                });
+
+                return false;
+
+            }
+        }
+
+        $$.ajax({
+            url: that.getRequestURLForXWL(options),
+            method: options.method,
+            data: options.data,
+            success:function(data){
+                data = data ? JSON.parse(data) : '';
+
+                var codes = [
+                    {code:10000, message:'Your session is invalid, please login again',path:'/'},
+                    {code:10001, message:'Unknown error,please login again',path:'tpl/login.html'},
+                    {code:20001, message:'User name or password does not match',path:'/'}
+                ];
+
+                var codeLevel = that.search(data.error_code, codes);
+
+                if(!codeLevel){
+                    (typeof (callback) === 'function') ? callback(data) : '';
+                }else{
+                    hiApp.alert(codeLevel.message, function () {
+                        hiApp.hideIndicator();
+                        hiApp.hidePreloader();
+                    });
+                }
+            }
+        });
+    },
+
     simpleCall: function(options,callback){
         var that = this;
 
